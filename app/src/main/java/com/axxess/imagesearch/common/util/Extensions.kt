41932889c6175487f9a66088089ad5bc84ef0e6f
Anand Shinde <anand.shinde.2@team.telstra.com>
2020-07-31 15:22:32 +1000

    - Added extension functions to load images with circular progress bar.

--- app/src/main/java/com/axxess/imagesearch/common/util/Extensions.kt
+++ app/src/main/java/com/axxess/imagesearch/common/util/Extensions.kt
@@ -4,14 +4,17 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.axxess.imagesearch.R
+import com.squareup.picasso.Picasso
 
 /**
  * File to write all extension functions.
  * @author Anand Shinde
  */
-
 
 fun View.show() {
     this.visibility = View.VISIBLE
@@ -33,4 +36,21 @@ fun Activity.hideKeyboard() {
 fun Activity.showKeyboard() {
     val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
+}
+
+fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+fun ImageView.loadImage(url: String?, progressDrawable: CircularProgressDrawable) {
+    Picasso.get().load(url).fit().placeholder(progressDrawable)
+        .error(R.drawable.ic_download_error).into(this)
+}
+
+fun ImageView.loadImage(url: String?) {
+    loadImage(url, getProgressDrawable(this.context))
 }
