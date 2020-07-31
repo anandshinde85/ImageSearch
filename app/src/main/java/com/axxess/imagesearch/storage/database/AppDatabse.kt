0be17e08413ffbdfab39f4a4b73c3b3cb54ec47b
Anand Shinde <anand.shinde.2@team.telstra.com>
2020-07-31 23:15:05 +1000

    - Added Comment table.

--- app/src/main/java/com/axxess/imagesearch/storage/database/AppDatabse.kt
+++ app/src/main/java/com/axxess/imagesearch/storage/database/AppDatabse.kt
@@ -1,20 +1,23 @@
 package com.axxess.imagesearch.storage.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.anand.mvvmskeletonarchitecture.storage.ApiCache
-import com.axxess.imagesearch.storage.BaseDatabase
+import com.anand.mvvmskeletonarchitecture.storage.ApiCacheDao
 import com.axxess.imagesearch.storage.DateConverter
+import com.axxess.imagesearch.storage.comments.Comment
+import com.axxess.imagesearch.storage.comments.CommentsDao
 
 const val DATABASE_NAME = "appdb"
 
 @Database(
-    entities = [ApiCache::class],
+    entities = [Comment::class, ApiCache::class],
     version = 1,
     exportSchema = false
 )
-@TypeConverters(
-    DateConverter::class
-)
-abstract class AppDatabase : BaseDatabase() {
+@TypeConverters(DateConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun apiCacheDao(): ApiCacheDao
+    abstract fun commentsDao(): CommentsDao
 }
