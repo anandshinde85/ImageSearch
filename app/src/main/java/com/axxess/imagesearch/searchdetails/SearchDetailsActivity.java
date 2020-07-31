ddd804c48cbb2dc40854a23ef2bcef63f0165365
Anand Shinde <anand.shinde.2@team.telstra.com>
2020-07-31 16:05:17 +1000

    - Displayed passed search result.

--- app/src/main/java/com/axxess/imagesearch/searchdetails/SearchDetailsActivity.java
+++ app/src/main/java/com/axxess/imagesearch/searchdetails/SearchDetailsActivity.java
@@ -3,21 +3,30 @@ package com.axxess.imagesearch.searchdetails;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 
 import com.axxess.imagesearch.R;
 import com.axxess.imagesearch.common.ui.BaseActivity;
+import com.axxess.imagesearch.common.util.Utility;
 import com.axxess.imagesearch.networking.imagesearch.SearchResults;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class SearchDetailsActivity extends BaseActivity {
-    public static String SEARCH_RESULTS_ARG = "search_results";
-    public static String SEARCH_STRING_ARG = "search_string";
+    public static final String SEARCH_RESULTS_ARG = "search_results";
+    public static final String SEARCH_STRING_ARG = "search_string";
     private SearchResults searchResults;
     private String searchString;
+    private FrameLayout contentFrame;
+    private ProgressBar progressBar;
+    private ImageView imageDetail;
+    private TextView oldCommentTitle, oldComment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,21 +37,31 @@ public class SearchDetailsActivity extends BaseActivity {
             searchString = intent.getStringExtra(SEARCH_STRING_ARG);
         }
         setUpActionBar();
+        loadSearchDetails();
+    }
+
+    private void loadSearchDetails() {
+        if (searchResults != null && !searchResults.getImages().isEmpty()) {
+            Utility.loadImage(imageDetail, searchResults.getImages().get(0).getLink());
+        }
     }
 
     @Override
     public void showContent() {
-
+        Utility.show(contentFrame);
+        Utility.hide(progressBar);
     }
 
     @Override
     public void showLoading() {
-
+        Utility.hide(contentFrame);
+        Utility.show(progressBar);
     }
 
     @Override
     public void hideLoading() {
-
+        Utility.show(contentFrame);
+        Utility.hide(progressBar);
     }
 
     @Override
@@ -62,6 +81,19 @@ public class SearchDetailsActivity extends BaseActivity {
 
     @Override
     public void setUpUI() {
+        initBaseLayout();
+        initDetailViews();
+    }
+
+    private void initDetailViews() {
+        imageDetail = findViewById(R.id.imageDetail);
+        oldCommentTitle = findViewById(R.id.oldCommentTitle);
+        oldComment = findViewById(R.id.oldComment);
+    }
+
+    private void initBaseLayout() {
+        contentFrame = findViewById(R.id.contentFrame);
+        progressBar = findViewById(R.id.baseProgressbar);
     }
 
     private void setUpActionBar() {
